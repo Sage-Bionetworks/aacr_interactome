8d74a9bdd506f8a4e9c53986ab28b05a721acc34
Thomas Corey <Thomas.corey@mail.mcgill.ca>
2019-10-11 15:43:21 -0700

    update travis and add 2 config files for smi and adc

--- code/create_interactome_files.R
+++ code/create_interactome_files.R
@@ -1,6 +1,19 @@
 library(dplyr)
 
-source("config.R")
+# source("config.R")
+## Running this file requires objects created with names
+# data_path (filename of data being used), 
+# json_dir (where to place the json files), 
+# json_path (json_dir + common name of the json files w/o the extension)
+# manipulate, a function which takes in a dataframe and returns a dataframe,
+#   containing any manipulations one would like to do on the data before
+#   creating the interactome
+# id_column, column name of id, which should be unique for each row
+# filter_columns, a vector of column names to allow users to filter by
+# filter_aliases, names we should call these filtered columns in the jsons
+# json_cols, vector of column values to include in the json objects
+# json_aliases, vector of names to call these values
+
 source("common.R")
 
 if(endsWith(data_path, ".xlsx")) {
@@ -18,10 +31,10 @@ tag_data <- manipulate(tag_data)
 
 # Create json folder inside interactome, for S3
 dir.create(json_dir, showWarnings = FALSE)
-existing_files <- paste(json_dir, list.files(path = json_dir),sep = "/")
-if(length(existing_files) > 1){
-  length(file.remove(existing_files))
-}
+#existing_files <- paste(json_dir, list.files(path = json_dir),sep = "/")
+#if(length(existing_files) > 1){
+#  length(file.remove(existing_files))
+#}
 
 # Export abstracts to individual jsons to be displayed
 # when the leaves are clicked
